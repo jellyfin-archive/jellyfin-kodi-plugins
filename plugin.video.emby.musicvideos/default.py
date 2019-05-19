410dd01e7f235fd3cfb054416261059c2ba1193b
Joshua M. Boniface <joshua@boniface.me>
2019-05-19 17:37:33 -0400

    Add Kodi addons from beta 0.14 repository

---
+++ plugin.video.emby.musicvideos/default.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+#################################################################################################
+
+import os
+import sys
+import urlparse
+
+import xbmc
+import xbmcaddon
+
+#################################################################################################
+
+__addon__ = xbmcaddon.Addon(id='plugin.video.emby')
+__base__ = xbmc.translatePath(os.path.join(__addon__.getAddonInfo('path'), 'resources', 'lib')).decode('utf-8')
+__libraries__ = xbmc.translatePath(os.path.join(__addon__.getAddonInfo('path'), 'libraries')).decode('utf-8')
+__pcache__ = xbmc.translatePath(os.path.join(__addon__.getAddonInfo('profile'), 'emby')).decode('utf-8')
+__cache__ = xbmc.translatePath('special://temp/emby').decode('utf-8')
+
+sys.path.insert(0, __cache__)
+sys.path.insert(0, __pcache__)
+sys.path.insert(0, __libraries__)
+sys.path.append(__base__)
+
+#################################################################################################
+
+from entrypoint import Events
+
+#################################################################################################
+
+Events()
